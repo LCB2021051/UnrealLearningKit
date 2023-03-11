0c1d35da797256784735027ac7bff1b88a61f982
Vivek Korah <lcb2021051@iiitl.ac.in>
2023-03-11 14:54:09 +0530

    Added rest of the file to the folder

---
+++ Source/UnrealLearningKit/FloatingPlatform2.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingPlatform2.h"
+
+// Sets default values
+AFloatingPlatform2::AFloatingPlatform2()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFloatingPlatform2::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingPlatform2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
